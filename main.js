59a370801bee3e3736a843dd479f56f389ba4ddd
IlimaG <ivanilimag@gmail.com>
2022-11-17 21:23:51 +0100

    Ejercicios

---
+++ main.js
@@ -0,0 +1,67 @@
+
+
+function costeHotel(num) {
+    return num * 140
+}
+
+
+function costeAlquilerCoche(num) {
+    let result = 0
+
+    if (num >= 3 && num <= 6) {
+        result = (num * 40) - 20
+        return result
+    }
+    if (num >= 7) {
+        result = (num * 40) - 50
+        return result
+    }
+}
+
+function costeAvion(city, num) {
+    let result = 0
+
+    if (city === "Oviedo") {
+        result = num * 15
+    } else if (city === "Tokyo") {
+        result = num * 700
+    } else if (city === "Madrid" || city === "Barcelona") {
+        result = num * 90
+    }
+
+    if (num > 3) {
+        return result - (result * 0.1)
+    } else {
+        return result
+    }
+}
+
+const numNights = document.querySelector("#nights")
+const city = document.querySelector("#citys")
+const calculateCost = document.querySelector("#calculate")
+const hotelCost = document.querySelector("#hotel")
+const travelCost = document.querySelector("#travel")
+const carCost = document.querySelector("#car")
+const totalCost = document.querySelector("#TotalCost")
+
+
+function caluclarCoste(city1, num) {
+    city1 = city.value
+    num = nights.value
+    
+    calculateCost.onclick = () => {
+    costeHotel(num)
+    costeAlquilerCoche(num)
+    costeAvion(city1, num)
+
+    hotelCost.value = costeHotel(num)
+}
+
+}
+
+
+
+// calculateCost.onclick = () => {
+    
+// }
+
